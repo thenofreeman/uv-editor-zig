9070971e14ba70c45fdc1fd8bed72d064579c9d9
nolan freeman abbott <nolan@freemanabbott.com>
2025-03-26 16:59:04 -0500

    add doc comments

--- src/buffer.zig
+++ src/buffer.zig
@@ -3,27 +3,34 @@ const std = @import("std");
 const Location = @import("location.zig").Location;
 
 pub const Buffer = struct {
+    /// A string name for users to refer to the buffer by
     name: []const u8,
 
+    /// Buffer Gap positions
     gapStart: usize,
     gapEnd: usize,
 
+    /// The current location where edit operations take place.
     point: Location,
-
+    /// Tracks current line position efficiently
     currentLine: usize,
 
+    /// Tracks the buffer total char/line count efficiently
     numChars: usize,
     numLines: usize,
 
     // markTree: AVLTree(Mark),
 
-    contents: []const u8, // Storage
-
+    // Name of the file on disk. Empty string if none yet given
     // fileName: []const u8,
+    // The last time in which the buffer and the associated file were identical
+    // Helps determine if a file was altered by another process (ie editing out of sync)
     // fileTime: u64,
-
+    // Has the editor modifed the buffer since it was last written or read
     // isModified: bool,
 
+    // List of active modes
+    // Must be sorted
     // modeList: *Mode,
 
     pub fn init(allocator: std.mem.Allocator, name: []const u8) !Buffer {
